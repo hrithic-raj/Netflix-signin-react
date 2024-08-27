5e3a1a0055873a7b624bb41b45e5854f87edabe4
Hrithic <hrithicraj730@gmail.com>
2024-08-27 10:19:47 +0530

    netflix

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Formval from './formval'
+import Navbar from './Navbar';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Formval/>
+    </div>
+  );
+}
+
+export default App;
